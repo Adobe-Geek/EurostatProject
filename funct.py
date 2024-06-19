c81c756a3d38524ba9d306d4ff57ba0ad3b2a8e9
Ania Komarova <anita.komarova@gmail.com>
2024-06-20 00:19:23 +0200

    added DB connection handling here

--- funct.py
+++ funct.py
@@ -1,6 +1,36 @@
 import json
 import psycopg2
+import time
 from psycopg2 import sql
+
+
+def connect_to_postgres():
+    host = "localhost"
+    database = "postgres"
+    user = "postgres"
+    password = "123"
+
+    attempt = 0
+    max_attempts = 10
+    interval = 60  # 1 minute
+
+    while attempt < max_attempts:
+        try:
+            connection = psycopg2.connect(
+                host=host, database=database, user=user, password=password
+            )
+            print("Connection successful")
+            return connection
+        except psycopg2.OperationalError as e:
+            attempt += 1
+            print(f"Attempt {attempt} failed: {e}")
+            if attempt < max_attempts:
+                print(f"Retrying in {interval} seconds...")
+                time.sleep(interval)
+
+    print("Server is down")
+    return None
+
 
 ind = {}
 lab = {}
@@ -15,19 +45,22 @@ extr_data["id"] = list(ind.values())
 extr_data["code"] = list(ind.keys())
 extr_data["country"] = list(lab.values())
 
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=123")
-cur = conn.cursor()
+conn = connect_to_postgres()
+if conn:
+    cur = conn.cursor()
 
-insert_query = "INSERT INTO core (id, code, country) VALUES (%s, %s, %s)"
+    insert_query = "INSERT INTO core (id, code, country) VALUES (%s, %s, %s)"
 
-for i in range(len(extr_data["id"])):
-    cur.execute(
-        insert_query,
-        (extr_data["id"][i], extr_data["code"][i], extr_data["country"][i]),
-    )
+    for i in range(len(extr_data["id"])):
+        cur.execute(
+            insert_query,
+            (extr_data["id"][i], extr_data["code"][i], extr_data["country"][i]),
+        )
 
-# Commit the transaction
-conn.commit()
+    # Commit the transaction
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 def process_json(file_paths):
@@ -68,27 +101,20 @@ def main():
     ]
     results = process_json(file_paths)
 
-    conn_params = {
-        "host": "localhost",
-        "database": "postgres",
-        "user": "postgres",
-        "password": "123",
-    }
-
-    conn = psycopg2.connect(**conn_params)
-
-    try:
-        table_names_and_columns = [
-            ("unemployment", ("id", "unemployment")),
-            ("vacancy", ("id", "vacant")),
-            ("population", ("id", "population")),
-        ]
-        for i, result in enumerate(results):
-            table_name, columns = table_names_and_columns[i]
-            insert_data_to_db(table_name, columns, result, conn)
-            print(f"Inserted data from file {i+1} into {table_name}")
-    finally:
-        conn.close()
+    conn = connect_to_postgres()
+    if conn:
+        try:
+            table_names_and_columns = [
+                ("unemployment", ("id", "unemployment")),
+                ("vacancy", ("id", "vacant")),
+                ("population", ("id", "population")),
+            ]
+            for i, result in enumerate(results):
+                table_name, columns = table_names_and_columns[i]
+                insert_data_to_db(table_name, columns, result, conn)
+                print(f"Inserted data from file {i+1} into {table_name}")
+        finally:
+            conn.close()
 
 
 if __name__ == "__main__":
